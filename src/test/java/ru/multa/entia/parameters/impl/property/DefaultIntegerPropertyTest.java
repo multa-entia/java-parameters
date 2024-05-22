47481008347d6559a84119e76cb8bc436cc3f777
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-05-22 22:41:55 +0300

    DefaultIntegerProperty is done

--- src/test/java/ru/multa/entia/parameters/impl/property/DefaultIntegerPropertyTest.java
+++ src/test/java/ru/multa/entia/parameters/impl/property/DefaultIntegerPropertyTest.java
@@ -1,101 +1,66 @@
-//package ru.multa.entia.parameters.impl.property;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvSource;
-//import org.mockito.Mockito;
-//import ru.multa.entia.fakers.impl.Faker;
-//import ru.multa.entia.parameters.api.property.Property;
-//import ru.multa.entia.results.api.repository.CodeRepository;
-//import ru.multa.entia.results.api.result.Result;
-//import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
-//import ru.multa.entia.results.impl.result.DefaultResultBuilder;
-//import ru.multa.entia.results.utils.Results;
-//
-//import java.util.function.Supplier;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class DefaultIntegerPropertyTest {
-//    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
-//
-//    @Test
-//    void shouldCheckGetting_ifInnerPropertyFail() {
-//        String expectedCode = Faker.str_().random();
-//        Supplier<Property<Object>> propertySupplier = () -> {
-//            Result<Object> result = DefaultResultBuilder.<Object>fail(expectedCode);
-//            DefaultPropertyOld property = Mockito.mock(DefaultPropertyOld.class);
-//            Mockito.when(property.get()).thenReturn(result);
-//
-//            return property;
-//        };
-//
-//        Result<Integer> result = new DefaultIntegerPropertyOld(propertySupplier.get()).get();
-//
-//        assertThat(
-//                Results.comparator(result)
-//                        .isFail()
-//                        .value(null)
-//                        .seedsComparator()
-//                        .code(expectedCode)
-//                        .back()
-//                        .compare()
-//        ).isTrue();
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource(value = {
-//            ",",
-//            "abc,",
-//            "12.3.4.5,"
-//    }, nullValues = "")
-//    void shouldCheckGetting_ifParsingFail(String raw) {
-//        Supplier<Property<Object>> propertySupplier = () -> {
-//            Result<Object> result = DefaultResultBuilder.<Object>ok(raw);
-//            DefaultPropertyOld property = Mockito.mock(DefaultPropertyOld.class);
-//            Mockito.when(property.get()).thenReturn(result);
-//
-//            return property;
-//        };
-//
-//        Result<Integer> result = new DefaultIntegerPropertyOld(propertySupplier.get()).get();
-//
-//        assertThat(
-//                Results.comparator(result)
-//                        .isFail()
-//                        .value(null)
-//                        .seedsComparator()
-//                        .code(CR.get(DefaultIntegerPropertyOld.Code.PARSE_ERROR))
-//                        .back()
-//                        .compare()
-//        ).isTrue();
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource(value = {
-//            "0,0",
-//            "11,11",
-//            "-123,-123"
-//    }, nullValues = "")
-//    void shouldCheckGetting(String raw, int expected) {
-//        Supplier<Property<Object>> propertySupplier = () -> {
-//            Result<Object> result = DefaultResultBuilder.<Object>ok(raw);
-//            DefaultPropertyOld property = Mockito.mock(DefaultPropertyOld.class);
-//            Mockito.when(property.get()).thenReturn(result);
-//
-//            return property;
-//        };
-//
-//        Result<Integer> result = new DefaultIntegerPropertyOld(propertySupplier.get()).get();
-//
-//        assertThat(
-//                Results.comparator(result)
-//                        .isSuccess()
-//                        .value(expected)
-//                        .seedsComparator()
-//                        .isNull()
-//                        .back()
-//                        .compare()
-//        ).isTrue();
-//    }
-//}
+package ru.multa.entia.parameters.impl.property;
+
+import org.junit.jupiter.api.Test;
+import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.results.api.repository.CodeRepository;
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.utils.Results;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DefaultIntegerPropertyTest {
+    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
+
+    @Test
+    void shouldCheckGetting_ifWasSetNull() {
+        DefaultIntegerProperty property = new DefaultIntegerProperty(Faker.str_().random());
+        property.set(null);
+        Result<Integer> result = property.get();
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(DefaultIntegerProperty.Code.IS_NULL))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckGetting_ifWasSetInvalidObject() {
+        DefaultIntegerProperty property = new DefaultIntegerProperty(Faker.str_().random());
+        property.set(Faker.str_().fromTemplate("[a-b]{10:20}"));
+        Result<Integer> result = property.get();
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(DefaultIntegerProperty.Code.INVALID_RAW))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckGetting() {
+        Integer expected = Faker.int_().between(-100, 100);
+        DefaultIntegerProperty property = new DefaultIntegerProperty(Faker.str_().random());
+        property.set(String.valueOf(expected));
+        Result<Integer> result = property.get();
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .value(expected)
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+}
