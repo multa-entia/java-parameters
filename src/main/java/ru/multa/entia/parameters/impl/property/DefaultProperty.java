786492de6480081363566ee806d5e6d146efb232
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-05-22 22:46:36 +0300

    DefaultProperty is done

---
+++ src/main/java/ru/multa/entia/parameters/impl/property/DefaultProperty.java
@@ -0,0 +1,16 @@
+package ru.multa.entia.parameters.impl.property;
+
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
+
+public class DefaultProperty extends AbstractProperty<Object> {
+
+    public DefaultProperty(final String name) {
+        super(name);
+    }
+
+    @Override
+    protected Result<Object> checkRaw(final Object raw) {
+        return DefaultResultBuilder.<Object>ok(raw);
+    }
+}
