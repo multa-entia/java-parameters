612b565363628779f43d6e91575bcbeab6fb4f9d
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-05-22 22:54:26 +0300

    DefaultNotNullProperty is done

---
+++ src/main/java/ru/multa/entia/parameters/impl/property/DefaultNotNullProperty.java
@@ -0,0 +1,29 @@
+package ru.multa.entia.parameters.impl.property;
+
+import ru.multa.entia.results.api.repository.CodeRepository;
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
+
+public class DefaultNotNullProperty extends AbstractProperty<Object>{
+    public enum Code {
+        IS_NULL
+    }
+
+    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
+    static {
+        CR.update(DefaultIntegerProperty.Code.IS_NULL, "parameters:not-null-property.default:is-null");
+    }
+
+    public DefaultNotNullProperty(final String name) {
+        super(name);
+    }
+
+    @Override
+    protected Result<Object> checkRaw(final Object raw) {
+        return DefaultResultBuilder.<Object>computeFromCodes(
+                () -> {return raw;},
+                () -> {return raw == null ? CR.get(Code.IS_NULL) : null;}
+        );
+    }
+}
