0cb27f6845ce9a6fd49adf4822ddfa8af0733793
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-05-25 17:55:55 +0300

    DefaultTextualPropertySource is done

--- src/main/java/ru/multa/entia/parameters/impl/source/DefaultTextualPropertySource.java
+++ src/main/java/ru/multa/entia/parameters/impl/source/DefaultTextualPropertySource.java
@@ -1,28 +1,80 @@
 package ru.multa.entia.parameters.impl.source;
 
-import lombok.RequiredArgsConstructor;
+import ru.multa.entia.parameters.api.reader.file.ReadResult;
 import ru.multa.entia.parameters.api.reader.file.Reader;
 import ru.multa.entia.parameters.api.source.PropertySource;
+import ru.multa.entia.parameters.api.source.SourceAdapter;
+import ru.multa.entia.results.api.repository.CodeRepository;
 import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
-@RequiredArgsConstructor
+
 public class DefaultTextualPropertySource implements PropertySource {
+    public enum Code {
+        READER_IS_NULL,
+        NOT_INIT,
+        NOT_PRESENT
+    }
+
+    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
+    static {
+        CR.update(Code.READER_IS_NULL, "parameters:property-source.textual.default:reader-is-null");
+        CR.update(Code.NOT_INIT, "parameters:property-source.textual.default:not-init");
+        CR.update(Code.NOT_PRESENT, "parameters:property-source.textual.default:not-present");
+    }
+
     private final Reader reader;
+    private final SourceAdapter<String, Map<String, Object>> adapter;
 
-    private Map<String, Object> rawProperties = new HashMap<>();
+    private Map<String, Object> rawProperties;
 
-    @Override
-    public Result<Object> update() {
-        // TODO: impl
-        return null;
+    public DefaultTextualPropertySource() {
+        this(null, null);
+    }
+
+    public DefaultTextualPropertySource(final Reader reader) {
+        this(reader, null);
+    }
+
+    public DefaultTextualPropertySource(final Reader reader,
+                                        final SourceAdapter<String, Map<String, Object>> adapter) {
+        this.reader = reader;
+        this.adapter = Objects.requireNonNullElse(adapter, new DefaultYamlSourceAdapter());
     }
 
     @Override
-    public Result<Object> get(final String propertyName) {
-        // TODO: impl
-        return null;
+    public Result<Object> update() {
+        if (reader == null) {
+            return DefaultResultBuilder.<Object>fail(CR.get(Code.READER_IS_NULL));
+        }
+
+        Result<ReadResult> readerResult = reader.read();
+        if (!readerResult.ok()) {
+            return DefaultResultBuilder.<Object>fail(readerResult.seed());
+        }
+
+        Result<Map<String, Object>> adapterResult = adapter.adapt(readerResult.value().content());
+        if (!adapterResult.ok()) {
+            return DefaultResultBuilder.<Object>fail(adapterResult.seed());
+        }
+
+        synchronized (this) {
+            rawProperties = adapterResult.value();
+        }
+        return DefaultResultBuilder.<Object>ok();
+    }
+
+    @Override
+    public synchronized Result<Object> get(final String propertyName) {
+        if (rawProperties == null) {
+            return DefaultResultBuilder.<Object>fail(CR.get(Code.NOT_INIT));
+        }
+        return rawProperties.containsKey(propertyName)
+                ? DefaultResultBuilder.<Object>ok(rawProperties.get(propertyName))
+                : DefaultResultBuilder.<Object>fail(CR.get(Code.NOT_PRESENT));
     }
 }
--- src/test/java/ru/multa/entia/parameters/impl/source/DefaultTextualPropertySourceTest.java
+++ src/test/java/ru/multa/entia/parameters/impl/source/DefaultTextualPropertySourceTest.java
@@ -1,36 +1,216 @@
 package ru.multa.entia.parameters.impl.source;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.parameters.api.reader.file.ReadResult;
+import ru.multa.entia.parameters.api.reader.file.Reader;
+import ru.multa.entia.parameters.api.source.SourceAdapter;
+import ru.multa.entia.parameters.impl.reader.file.DefaultReadResult;
+import ru.multa.entia.results.api.repository.CodeRepository;
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
+import ru.multa.entia.results.utils.Results;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultTextualPropertySourceTest {
+    private static final Function<String, Reader> READER_FUNCTION = content -> {
+        Result<ReadResult> result = DefaultResultBuilder.<ReadResult>ok(new DefaultReadResult(content, null, null));
+        Reader reader = Mockito.mock(Reader.class);
+        Mockito.when(reader.read()).thenReturn(result);
 
-    // TODO: !!!
-//    @Test
-//    void shouldCheckUpdating_ifReaderIsNull() {
-//    }
-//
-//    @Test
-//    void shouldCheckUpdating_ifReaderReturnedFail() {
-//
-//    }
-//
-//    @Test
-//    void shouldCheckUpdating_ifReaderReturnedInvalidString() {
-//
-//    }
-//
-//    @Test
-//    void shouldCheckUpdating() {
-//
-//    }
-//
-//    @Test
-//    void shouldCheckGetting_ifItIsAbsence() {
-//
-//    }
-//
-//    @Test
-//    void shouldCheckGetting() {
-//
-//    }
+        return reader;
+    };
+
+    private static final Function<Map<String, Object>, TestAdapter> ADAPTER_FUNCTION = map -> {
+        Result<Map<String, Object>> result = DefaultResultBuilder.<Map<String, Object>>ok(map);
+        TestAdapter adapter = Mockito.mock(TestAdapter.class);
+        Mockito.when(adapter.adapt(Mockito.any())).thenReturn(result);
+
+        return adapter;
+    };
+
+    private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
+
+    @Test
+    void shouldCheckUpdating_ifReaderIsNull() {
+        Result<Object> result = new DefaultTextualPropertySource(null).update();
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(DefaultTextualPropertySource.Code.READER_IS_NULL))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckUpdating_ifReaderReturnedFail() {
+        String expectedCode = Faker.str_().random();
+        Supplier<Reader> readerSupplier = () -> {
+            Result<ReadResult> result = DefaultResultBuilder.<ReadResult>fail(expectedCode);
+            Reader reader = Mockito.mock(Reader.class);
+            Mockito.when(reader.read()).thenReturn(result);
+
+            return reader;
+        };
+
+        Result<Object> result = new DefaultTextualPropertySource(readerSupplier.get()).update();
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(expectedCode)
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckUpdating_ifAdapterReturnedFail() {
+        String expectedCode = Faker.str_().random();
+        Supplier<TestAdapter> adapterSupplier = () -> {
+            Result<Map<String, Object>> result = DefaultResultBuilder.<Map<String, Object>>fail(expectedCode);
+            TestAdapter adapter = Mockito.mock(TestAdapter.class);
+            Mockito.when(adapter.adapt(Mockito.any())).thenReturn(result);
+
+            return adapter;
+        };
+
+        Result<Object> result = new DefaultTextualPropertySource(
+                READER_FUNCTION.apply(Faker.str_().random()),
+                adapterSupplier.get()
+        ).update();
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(expectedCode)
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @SneakyThrows
+    @Test
+    void shouldCheckUpdating() {
+        int quantity = Faker.int_().between(10, 20);
+        HashMap<String, Object> expected = new HashMap<>();
+        for (int i = 0; i < quantity; i++) {
+            expected.put(Faker.str_().random(), Faker.str_().random());
+        }
+
+        Supplier<TestAdapter> adapterSupplier = () -> {
+            Result<Map<String, Object>> result = DefaultResultBuilder.<Map<String, Object>>ok(expected);
+            TestAdapter adapter = Mockito.mock(TestAdapter.class);
+            Mockito.when(adapter.adapt(Mockito.any())).thenReturn(result);
+
+            return adapter;
+        };
+
+        DefaultTextualPropertySource source = new DefaultTextualPropertySource(
+                READER_FUNCTION.apply(Faker.str_().random()),
+                adapterSupplier.get()
+        );
+        Result<Object> result = source.update();
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .value(null)
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+
+        Field field = source.getClass().getDeclaredField("rawProperties");
+        field.setAccessible(true);
+        Object gotten = field.get(source);
+
+        assertThat(gotten).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldCheckGetting_ifItIsNotInit() {
+        DefaultTextualPropertySource source = new DefaultTextualPropertySource(
+                READER_FUNCTION.apply(Faker.str_().random()),
+                ADAPTER_FUNCTION.apply(Map.of())
+        );
+
+        Result<Object> result = source.get(Faker.str_().random());
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(DefaultTextualPropertySource.Code.NOT_INIT))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckGetting_ifItIsAbsence() {
+        String nonExistPropertyName = Faker.str_().random();
+        DefaultTextualPropertySource source = new DefaultTextualPropertySource(
+                READER_FUNCTION.apply(Faker.str_().random()),
+                ADAPTER_FUNCTION.apply(Map.of())
+        );
+        source.update();
+
+        Result<Object> result = source.get(nonExistPropertyName);
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(DefaultTextualPropertySource.Code.NOT_PRESENT))
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    @Test
+    void shouldCheckGetting() {
+        String propertyName = Faker.str_().random();
+        Object property = Faker.str_().random();
+        DefaultTextualPropertySource source = new DefaultTextualPropertySource(
+                READER_FUNCTION.apply(Faker.str_().random()),
+                ADAPTER_FUNCTION.apply(Map.of(propertyName, property))
+        );
+        source.update();
+
+        Result<Object> result = source.get(propertyName);
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .value(property)
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
+    }
+
+    private interface TestAdapter extends SourceAdapter<String, Map<String, Object>> {}
 }
