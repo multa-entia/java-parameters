d7da7ccccf9775707743c35e661fdaa9e13843ce
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-06-15 15:57:55 +0300

    DefaultYmlReader is done

--- src/main/java/ru/multa/entia/parameters/impl/reader/DefaultYmlReader.java
+++ src/main/java/ru/multa/entia/parameters/impl/reader/DefaultYmlReader.java
@@ -1,8 +1,12 @@
 package ru.multa.entia.parameters.impl.reader;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.error.YAMLException;
 import ru.multa.entia.parameters.api.ids.Id;
 import ru.multa.entia.parameters.api.reader.Reader;
+import ru.multa.entia.parameters.impl.ids.DefaultId;
 import ru.multa.entia.results.api.repository.CodeRepository;
 import ru.multa.entia.results.api.result.Result;
 import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
@@ -10,37 +14,63 @@ import ru.multa.entia.results.impl.result.DefaultResultBuilder;
 
 import java.nio.file.Path;
 import java.util.Map;
+import java.util.Objects;
 
 @EqualsAndHashCode
 public class DefaultYmlReader implements Reader<Map<String, Object>> {
     public enum Code {
         READER_OR_PATH_NOT_SET,
+        TEXT_READER_RET_FAIL,
         SYNTAX_ERROR
     }
 
     private static final CodeRepository CR = DefaultCodeRepository.getDefaultInstance();
     static {
         CR.update(Code.READER_OR_PATH_NOT_SET, "parameters:yml-reader.default:reader-or-path-not-set");
+        CR.update(Code.TEXT_READER_RET_FAIL, "parameters:yml-reader.default:text-reader-ret-fail");
         CR.update(Code.SYNTAX_ERROR, "parameters:yml-reader.default:syntax-error");
     }
+
+    private static final String PATH_TEMPLATE = "yml%s";
 
     public static Builder builder () {
         return new Builder();
     }
 
-    @Override
-    public Result<Map<String, Object>> read() {
-        return null;
+    private final Reader<String> textReader;
+    @Getter
+    private final Id id;
+
+    private DefaultYmlReader(final Reader<String> textReader,
+                            final Id id) {
+        this.textReader = textReader;
+        this.id = id;
     }
 
     @Override
-    public Id getId() {
-        return null;
+    public Result<Map<String, Object>> read() {
+        Result<String> result = textReader.read();
+        if (!result.ok()) {
+            return new DefaultResultBuilder<Map<String, Object>>()
+                    .success(false)
+                    .seedBuilder()
+                    .code(CR.get(Code.TEXT_READER_RET_FAIL))
+                    .apply()
+                    .causes(result)
+                    .build();
+        }
+
+        try {
+            return DefaultResultBuilder.<Map<String, Object>>ok(new Yaml().load(result.value()));
+        } catch (YAMLException ex) {
+            return DefaultResultBuilder.<Map<String, Object>>fail(CR.get(Code.SYNTAX_ERROR));
+        }
     }
 
     public static class Builder {
         private Reader<String> textReader;
         private Path path;
+        private Id id;
 
         public Builder textReader(final Reader<String> textReader) {
             this.textReader = textReader;
@@ -52,14 +82,20 @@ public class DefaultYmlReader implements Reader<Map<String, Object>> {
             return this;
         }
 
+        public Builder id (final Id id) {
+            this.id = id;
+            return this;
+        }
+
         public Result<Reader<Map<String, Object>>> build() {
-            if (textReader != null) {
-                return null;
-            } else if (path != null) {
-                return null;
+            id = Objects.requireNonNullElse(id, DefaultId.createIdForFile(Path.of(String.format(PATH_TEMPLATE, path))));
+            if (textReader == null && path != null) {
+                textReader = DefaultFileReader.builder().path(path).id(id).build().value();
             }
 
-            return DefaultResultBuilder.<Reader<Map<String, Object>>>fail(CR.get(Code.READER_OR_PATH_NOT_SET));
+            return textReader != null
+                    ? DefaultResultBuilder.<Reader<Map<String, Object>>>ok(new DefaultYmlReader(textReader, id))
+                    : DefaultResultBuilder.<Reader<Map<String, Object>>>fail(CR.get(Code.READER_OR_PATH_NOT_SET));
         }
     }
 }
--- src/test/java/ru/multa/entia/parameters/impl/reader/DefaultYmlReaderTest.java
+++ src/test/java/ru/multa/entia/parameters/impl/reader/DefaultYmlReaderTest.java
@@ -3,15 +3,20 @@ package ru.multa.entia.parameters.impl.reader;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.parameters.api.ids.Id;
 import ru.multa.entia.parameters.api.reader.Reader;
 import ru.multa.entia.results.api.repository.CodeRepository;
 import ru.multa.entia.results.api.result.Result;
 import ru.multa.entia.results.impl.repository.DefaultCodeRepository;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
 import ru.multa.entia.results.utils.Results;
 
 import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,6 +49,19 @@ class DefaultYmlReaderTest {
         assertThat(gotten).isEqualTo(expected);
     }
 
+    @SneakyThrows
+    @Test
+    void shouldCheckBuilder_idSetting() {
+        Id expectedId = Mockito.mock(Id.class);
+        DefaultYmlReader.Builder builder = DefaultYmlReader.builder().id(expectedId);
+
+        Field field = builder.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        Object gotten = field.get(builder);
+
+        assertThat(gotten).isEqualTo(expectedId);
+    }
+
     @Test
     void shouldCheckBuilding_ifReaderAndPathNotSet() {
         Result<Reader<Map<String, Object>>> result = DefaultYmlReader.builder().build();
@@ -59,34 +77,131 @@ class DefaultYmlReaderTest {
         ).isTrue();
     }
 
+    @SneakyThrows
     @Test
     void shouldCheckBuilding_ifOnlyPathSet() {
+        Path expected = Path.of("/opt");
+        Reader<Map<String, Object>> reader = DefaultYmlReader.builder().path(expected).build().value();
 
+        Field field = reader.getClass().getDeclaredField("textReader");
+        field.setAccessible(true);
+        Object gottenTextReader = field.get(reader);
+        field = gottenTextReader.getClass().getDeclaredField("path");
+        field.setAccessible(true);
+        Object gotten = field.get(gottenTextReader);
+
+        assertThat(gotten).isEqualTo(expected);
     }
 
+    @SneakyThrows
     @Test
     void shouldCheckBuilding() {
+        TestTextReader expectedTestReader = Mockito.mock(TestTextReader.class);
+        Reader<Map<String, Object>> reader = DefaultYmlReader.builder().textReader(expectedTestReader).build().value();
 
+        Field field = reader.getClass().getDeclaredField("textReader");
+        field.setAccessible(true);
+        Object gotten = field.get(reader);
+
+        assertThat(gotten).isEqualTo(expectedTestReader);
     }
 
     @Test
     void shouldCheckIdGetting() {
+        Id expectedId = Mockito.mock(Id.class);
+        Reader<Map<String, Object>> reader = DefaultYmlReader.builder().path(Path.of("/opt")).id(expectedId).build().value();
 
+        assertThat(reader.getId()).isEqualTo(expectedId);
     }
 
     @Test
     void shouldCheckReading_ifTextReaderRetFail() {
+        Supplier<Reader<String>> readerSupplier = () -> {
+            Result<String> result = DefaultResultBuilder.<String>fail(Faker.str_().random());
+            TestTextReader reader = Mockito.mock(TestTextReader.class);
+            Mockito.when(reader.read()).thenReturn(result);
 
+            return reader;
+        };
+
+        Reader<Map<String, Object>> reader = DefaultYmlReader.builder().textReader(readerSupplier.get()).build().value();
+        Result<Map<String, Object>> result = reader.read();
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(DefaultYmlReader.Code.TEXT_READER_RET_FAIL))
+                        .back()
+                        .compare()
+        ).isTrue();
     }
 
     @Test
     void shouldCheckReading_whenSyntaxError() {
+        String text =
+                """
+                intValue: 123doubleValue: 123.45
+                """;
+        Supplier<Reader<String>> textReaderSupplier = () -> {
+            Result<String> result = DefaultResultBuilder.<String>ok(text);
+            TestTextReader reader = Mockito.mock(TestTextReader.class);
+            Mockito.when(reader.read()).thenReturn(result);
 
+            return reader;
+        };
+
+        Reader<Map<String, Object>> reader = DefaultYmlReader.builder().textReader(textReaderSupplier.get()).build().value();
+        Result<Map<String, Object>> result = reader.read();
+
+        assertThat(
+                Results.comparator(result)
+                        .isFail()
+                        .value(null)
+                        .seedsComparator()
+                        .code(CR.get(DefaultYmlReader.Code.SYNTAX_ERROR))
+                        .back()
+                        .compare()
+        ).isTrue();
     }
 
     @Test
     void shouldCheckReading() {
+        String key0 = "intValue";
+        String key1 = "doubleValue";
+        int value0 = 123;
+        double value1 = 123.45;
+        String template =
+                """
+                %s: %s
+                %s: %s
+                """;
+        String text = String.format(template, key0, value0, key1, value1);
 
+        Supplier<Reader<String>> textReaderSupplier = () -> {
+            Result<String> result = DefaultResultBuilder.<String>ok(text);
+            TestTextReader reader = Mockito.mock(TestTextReader.class);
+            Mockito.when(reader.read()).thenReturn(result);
+
+            return reader;
+        };
+
+        Reader<Map<String, Object>> reader = DefaultYmlReader.builder().textReader(textReaderSupplier.get()).build().value();
+        Result<Map<String, Object>> result = reader.read();
+
+        assertThat(
+                Results.comparator(result)
+                        .isSuccess()
+                        .value(new HashMap<>(){{
+                            put(key0, value0);
+                            put(key1, value1);
+                        }})
+                        .seedsComparator()
+                        .isNull()
+                        .back()
+                        .compare()
+        ).isTrue();
     }
 
     private interface TestTextReader extends Reader<String> {}
